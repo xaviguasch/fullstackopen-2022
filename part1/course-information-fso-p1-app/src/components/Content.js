3946599a36fafac2526860ded5dfef18000d193b
xaviguasch <xaviguasch@gmail.com>
2022-06-13 10:20:44 +0200

    solved exercise 1.3: course information step3

--- part1/course-information-fso-p1-app/src/components/Content.js
+++ part1/course-information-fso-p1-app/src/components/Content.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 import Part from './Part'
 
-const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
+const Content = ({ part1, part2, part3 }) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </div>
   )
 }
