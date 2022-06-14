55cc5c30907e519cc9a634c6a4477068f1225ecf
xaviguasch <xaviguasch@gmail.com>
2022-06-14 12:05:50 +0200

    solved exercise 1.6: unicafe step1

--- part1/unicafe-fso-p1-app/src/App.js
+++ part1/unicafe-fso-p1-app/src/App.js
@@ -1,11 +1,14 @@
+import { useState } from 'react'
+
 import './App.css'
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Unicafe</h1>
-    </div>
-  )
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return <div>code here</div>
 }
 
 export default App
