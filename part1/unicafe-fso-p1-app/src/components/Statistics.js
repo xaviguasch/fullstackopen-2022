ecd7a6bdd428402313278c891227246e2fc4ccbc
xaviguasch <xaviguasch@gmail.com>
2022-06-14 12:59:43 +0200

    solved exercise 1.7: unicafe step2

--- part1/unicafe-fso-p1-app/src/components/Statistics.js
+++ part1/unicafe-fso-p1-app/src/components/Statistics.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / all
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
