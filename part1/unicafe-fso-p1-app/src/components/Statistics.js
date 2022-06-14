070d16b925cfcfad64d8525df15c6361bbaa2628
xaviguasch <xaviguasch@gmail.com>
2022-06-14 13:05:48 +0200

    solved exercise 1.8: unicafe step3 and also solved exercise 1.9: unicafe step4

--- part1/unicafe-fso-p1-app/src/components/Statistics.js
+++ part1/unicafe-fso-p1-app/src/components/Statistics.js
@@ -5,6 +5,14 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good * 1 + neutral * 0 + bad * -1) / all
   const positive = (good / all) * 100
 
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>Statistics</h2>
