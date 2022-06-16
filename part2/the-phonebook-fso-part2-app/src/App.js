106a064d4f491f5541c05a4b95dfc6122fd28fd1
xaviguasch <xaviguasch@gmail.com>
2022-06-16 12:13:04 +0200

    solved exercise 2.6: The Phonebook Step1

--- part2/the-phonebook-fso-part2-app/src/App.js
+++ part2/the-phonebook-fso-part2-app/src/App.js
@@ -1,9 +1,40 @@
+import { useState } from 'react'
+
 import './App.css'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleNoteChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    setPersons([...persons, { name: newName, id: persons.length + 1 }])
+
+    setNewName('')
+  }
+
   return (
-    <div className='App'>
-      <h1>The Phonebook</h1>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((p) => (
+          <li key={p.name}>{p.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
