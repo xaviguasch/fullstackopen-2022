23ef70f1cb1449f0c0501e637098c827e92f8306
xaviguasch <xaviguasch@gmail.com>
2022-06-16 13:13:46 +0200

    solved exercise 2.8: The Phonebook Step3

--- part2/the-phonebook-fso-part2-app/src/App.js
+++ part2/the-phonebook-fso-part2-app/src/App.js
@@ -6,11 +6,17 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
 
+  const [newNumber, setNewNumber] = useState('')
+
   const checkRepeats = (formName) =>
     persons.some((p) => p.name.toLowerCase() === formName.toLowerCase())
 
   const handleNoteChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addNameHandler = (event) => {
@@ -19,10 +25,14 @@ const App = () => {
     if (checkRepeats(newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons([...persons, { name: newName, id: persons.length + 1 }])
+      setPersons([
+        ...persons,
+        { name: newName, number: newNumber, id: persons.length + 1 },
+      ])
     }
 
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -33,13 +43,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNoteChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((p) => (
-          <li key={p.name}>{p.name}</li>
+          <li key={p.name}>
+            {p.name} {p.number}
+          </li>
         ))}
       </ul>
     </div>
