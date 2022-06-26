3a24305ee989dc9e2f08743117aadee6a56b0737
xaviguasch <xaviguasch@gmail.com>
2022-06-26 11:08:43 +0200

    solved exercise 2.20*: Phonebook step12

--- part2/the-phonebook-fso-part2-app/src/App.js
+++ part2/the-phonebook-fso-part2-app/src/App.js
@@ -6,6 +6,7 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import SuccessMessage from './components/SuccessMessage'
+import ErrorMessage from './components/ErrorMessage'
 
 import personService from './services/persons'
 
@@ -16,6 +17,7 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
   const [successMsg, setSuccessMsg] = useState(null)
+  const [errorMsg, setErrorMsg] = useState(null)
 
   useEffect(() => {
     personService.getAll().then((response) => {
@@ -52,18 +54,30 @@ const App = () => {
         )
         const updatedPerson = { ...personToUpdate, number: newNumber }
 
-        personService.updatePhone(updatedPerson.id, updatedPerson).then((response) => {
-          setPersons(
-            persons.map((person) =>
-              person.id !== updatedPerson.id ? person : updatedPerson
+        personService
+          .updatePhone(updatedPerson.id, updatedPerson)
+          .then((response) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== updatedPerson.id ? person : updatedPerson
+              )
             )
-          )
 
-          setSuccessMsg(`Updated ${updatedPerson.name}'s number`)
-          setTimeout(() => {
-            setSuccessMsg(null)
-          }, 5000)
-        })
+            setSuccessMsg(`Updated ${updatedPerson.name}'s number`)
+            setTimeout(() => {
+              setSuccessMsg(null)
+            }, 5000)
+          })
+          .catch((error) => {
+            setErrorMsg(
+              `Information of ${updatedPerson.name} has already been removed from the server`
+            )
+            setTimeout(() => {
+              setErrorMsg(null)
+            }, 5000)
+
+            setPersons(persons.filter((person) => person.id !== updatedPerson.id))
+          })
       }
     } else {
       const newContact = { name: newName, number: newNumber }
@@ -84,9 +98,17 @@ const App = () => {
     const personToDelete = persons.find((p) => p.id === id)
 
     if (window.confirm(`Delete ${personToDelete.name}`)) {
-      personService.deleteItem(id).then((response) => console.log(response))
+      personService.deleteItem(id).then((response) => {
+        console.log(response)
+        setPersons(persons.filter((p) => p.id !== id))
 
-      setPersons(persons.filter((p) => p.id !== id))
+        // SHOW SUCCESSFUL MESSAGE
+
+        setSuccessMsg(`Deleted ${personToDelete.name} from database`)
+        setTimeout(() => {
+          setSuccessMsg(null)
+        }, 5000)
+      })
     }
   }
 
@@ -94,6 +116,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <SuccessMessage message={successMsg} />
+      <ErrorMessage message={errorMsg} />
+
       <Filter onFilterText={filterText} onHandleFilterChange={handleFilterChange} />
 
       <h3>Add a new</h3>
