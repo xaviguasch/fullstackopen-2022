f3b974fedf2f6973fbdefa3e43e953dc9c5cb39c
xaviguasch <xaviguasch@gmail.com>
2022-06-29 23:33:08 +0200

    solved exercise 3.3: Phonebook backend step3

--- part3/the-phonebook-backend-fso-part3-app/index.js
+++ part3/the-phonebook-backend-fso-part3-app/index.js
@@ -32,6 +32,19 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    const statusMessage = "We haven't found any person with that ID"
+    res.status(404).end(statusMessage)
+  }
+})
+
 app.get('/info', (req, res) => {
   const numOfEntries = persons.length
   const currentDate = new Date()
