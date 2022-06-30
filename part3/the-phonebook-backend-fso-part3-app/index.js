9da05a86a2a2aa421f9c8bdd95eefee577769936
xaviguasch <xaviguasch@gmail.com>
2022-06-30 18:42:06 +0200

    solved exercise 3.5: Phonebook backend step5

--- part3/the-phonebook-backend-fso-part3-app/index.js
+++ part3/the-phonebook-backend-fso-part3-app/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -43,6 +45,14 @@ app.get('/api/persons/:id', (req, res) => {
     const statusMessage = "We haven't found any person with that ID"
     res.status(404).end(statusMessage)
   }
+})
+
+app.post('/api/persons', (req, res) => {
+  const person = { id: Math.floor(Math.random() * 1000000), ...req.body }
+
+  persons.push(person)
+
+  res.json(person)
 })
 
 app.get('/info', (req, res) => {
