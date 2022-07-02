bc0f4d7a0778068819deb586cfc74b9f721d9bb1
xaviguasch <xaviguasch@gmail.com>
2022-07-02 17:10:49 +0200

    solved exercise 3.8*: Phonebook backend step8

--- part3/the-phonebook-backend-fso-part3-app/index.js
+++ part3/the-phonebook-backend-fso-part3-app/index.js
@@ -28,7 +28,20 @@ let persons = [
   },
 ]
 
-app.use(morgan('tiny'))
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      JSON.stringify(req.body),
+    ].join(' ')
+  })
+)
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the phonebook!</h1>')
